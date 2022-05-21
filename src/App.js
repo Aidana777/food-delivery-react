02b1d0a4ca1e42c6e0ccc3c4db48de1b0a6ae66a
Aidana555 <aidananiazalieva27@gmail.com>
2022-05-21 12:18:46 +0600

    вввввв

--- src/App.js
+++ src/App.js
@@ -132,11 +132,7 @@ function App() {
 
           {!cart ? (
             <div className="addSomeItem">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2FemptyCart.png?alt=media&token=50b733d4-cdd9-4025-bffe-8efa4066ca24"
-                alt=""
-                className="emptyCart"
-              />
+      Empty
             </div>
           ) : (
             <div className="cartCheckOutContianer">
--- src/Components/DebitCard.js
+++ src/Components/DebitCard.js
@@ -9,14 +9,10 @@ function DebitCard() {
         className="cardLogo"
       />
 
-      <img
-        src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fchip.png?alt=media&token=401162f6-2dd2-4da4-bef7-6479c132789c"
-        alt=""
-        className="cardChip"
-      />
+    
 
-      <div className="card_number">1234 567 8920 3200</div>
-      <div className="card_name">Vetrivel Ravi</div>
+      <div className="card_number">2325466566887</div>
+      <div className="card_name"> App</div>
       <div className="card_from">10/21</div>
       <div className="card_to">10/25</div>
       <div className="card_ring"></div>
